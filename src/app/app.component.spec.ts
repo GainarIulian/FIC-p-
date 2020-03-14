9c2501bbb5fcaf7d0ba85ac2def2f142344a9fc0
Iulian Gainar <Iulian.Gainar@theaccessgroup.com>
2020-03-14 14:16:28 +0200

    Rename project to Learn

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Movie'`, () => {
+  it(`should have as title 'Learn'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Movie');
+    expect(app.title).toEqual('Learn');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Movie app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Learn app is running!');
   });
 });
