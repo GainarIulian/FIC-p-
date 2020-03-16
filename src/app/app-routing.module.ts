950c650b15b38a709a6ad5f17c222209fa4ab245
Iulian Gainar <Iulian.Gainar@theaccessgroup.com>
2020-03-17 01:02:16 +0200

    Add route to Product detail and small route guard example

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { ProductListComponent } from './products/product-list.component';
 import { ProductDetailComponent } from './products/product-detail.component';
 import { HomePageComponent } from './home/home-page.component';
+import { ProductDetailGuard } from './products/product-detail.guard';
 
 
 const routes: Routes = [
-  {path: 'products', component: ProductListComponent},
-  {path: 'products/:id', component: ProductDetailComponent},
-  {path: 'home', component: HomePageComponent},
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
-  {path: '**', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'products', component: ProductListComponent },
+  { path: 'products/:id', canActivate: [ProductDetailGuard], component: ProductDetailComponent },
+  { path: 'home', component: HomePageComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' },
 
 ];
 
